ce242b3f5bbc652e6e8bb649a4313318035156d9
Michael Light <mklight06@gmail.com>
2023-02-05 09:02:02 -0700

    successfully serving frontend

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,19 @@
 const Express = require('express');
+const path = require('path');
 
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
+app.use(Express.static(path.join(__dirname, '../../frontend/deploy')));
+app.use(Express.json());
+
 app.get('/', (req, res) => {
-  res.send('your app is connected');
+  res.sendFile(path.join(__dirname, '../../frontend/deploy/index.html'));
 });
+
+// app.get('/', (req, res) => {
+//   res.send('your app is connected');
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
