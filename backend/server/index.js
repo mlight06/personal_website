eb64d2793eaeb1831f660e64072db1f8a6cd0989
Michael Light <mklight06@gmail.com>
2023-02-05 08:55:24 -0700

    server connected

--- backend/server/index.js
+++ backend/server/index.js
@@ -3,6 +3,10 @@ const Express = require('express');
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('your app is connected');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
