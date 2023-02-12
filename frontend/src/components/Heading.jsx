941d1dacc616e940e8d8ab0f7999c688b7a0fdc9
Michael Light <mklight06@gmail.com>
2023-02-12 10:51:15 -0700

    added animations to Header

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { Paper } from '@mui/material';
 
 export default function Heading() {
   return (
     <div id="heading_main">
-      <motion.div
-        animate={{
-          x: ['0rem', '10rem', '-10rem', '0rem'],
-          y: ['0px', '20px', '0px'],
-        }}
-      >
-        <div id="introduction">
-          I`m Michael Light
-        </div>
-      </motion.div>
-      <motion.div
-        animate={{
-          x: ['0rem', '10rem', '-10rem', '0rem'],
-          y: ['0px', '20px', '0px'],
-        }}
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-      >
-        <div id="introduction">
-          and I`m a fullstack software developer
-        </div>
-      </motion.div>
+      <div>
+
+        <motion.div
+          animate={{
+            x: ['0rem', '10rem', '-10rem', '0rem'],
+            y: ['0px', '20px', '0px'],
+          }}
+        >
+          <Paper
+            id="introduction"
+            elevation={3}
+          >
+            <div>
+              I`m Michael Light
+            </div>
+          </Paper>
+        </motion.div>
+        <motion.div
+          animate={{
+            x: ['0rem', '-10rem', '10rem', '0rem'],
+            y: ['0px', '20px', '0px'],
+          }}
+        >
+          <Paper
+            elevation={3}
+          >
+            <div id="introduction">
+              and I`m a fullstack software developer
+            </div>
+          </Paper>
+        </motion.div>
+      </div>
     </div>
   );
 }
