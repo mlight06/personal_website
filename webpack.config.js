5fbe91fc06dae43a3d46c8b3a4ed729127d9008a
Michael Light <mklight06@gmail.com>
2023-09-26 08:51:21 -0600

    fixed webpack entry issue, image still not loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './frontend/src/index.js'),
-  },
+  // entry: {
+  //   main: path.resolve(__dirname, './frontend/src/index.js'),
+  // },
+  entry: './frontend/src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './frontend/deploy'),
