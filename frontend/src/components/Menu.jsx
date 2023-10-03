94e34a0343be5ade317126f763835d34ad4191ba
Michael Light <mklight06@gmail.com>
2023-10-03 09:01:02 -0600

    created accordion menu for homepage

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
+import {
+  Button, List, Divider, ListItem, Accordion, AccordionSummary, AccordionDetails,
+} from '@mui/material';
 // import Hobbies from './Hobbies';
 
 export default function Menu() {
   return (
     <div className="menu-container">
-      Menu
+      {/* Menu */}
       {/* <Hobbies /> */}
-      <div>
-        <Button variant="text">
-          <Link to="/hobbies">Hobbies</Link>
-        </Button>
-      </div>
-      <div>
-        <Button variant="text">
-          <Link to="/portfolio">Portfolio</Link>
-        </Button>
-      </div>
-      <div>
-        <Button variant="text">
-          <Link to="/projects">Projects</Link>
-        </Button>
-      </div>
+      {/* <div> */}
+      <Accordion>
+        <AccordionSummary>
+          Menu
+        </AccordionSummary>
+        <AccordionDetails>
+          <List>
+            {/* <ListItem>
+          Menu
+        </ListItem> */}
+            <ListItem>
+
+              <Button variant="text">
+                <Link to="/hobbies">Hobbies</Link>
+              </Button>
+            </ListItem>
+            <Divider />
+            {/* </div>
+      <div> */}
+            <ListItem>
+
+              <Button variant="text">
+                <Link to="/portfolio">Portfolio</Link>
+              </Button>
+            </ListItem>
+            <Divider />
+            {/* </div>
+      <div> */}
+            <ListItem>
+
+              <Button variant="text">
+                <Link to="/projects">Projects</Link>
+              </Button>
+            </ListItem>
+          </List>
+        </AccordionDetails>
+      </Accordion>
+      {/* </div> */}
     </div>
   );
 }
