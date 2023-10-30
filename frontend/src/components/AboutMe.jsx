0b40faf1cabb09f77f3a7a7c6aea5152563564ed
Michael Light <mklight06@gmail.com>
2023-10-30 11:30:44 -0600

    added gradient background colors to about me

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button, Paper, Typography } from '@mui/material';
+import { easeInOut, motion } from 'framer-motion';
+import { LinearScale } from '@mui/icons-material';
 import Menu from './Menu';
 
 export default function AboutMe() {
@@ -14,23 +16,45 @@ export default function AboutMe() {
           About Me
         </div>
         <div className="about_me_information">
+          <motion.div
+            animate={{ rotate: -360 }}
+            transition={{ type: 'spring' }}
+          >
+            <Paper elevation={3} className="about-me-paper" sx={{ background: 'linear-gradient(red, orange)', width: '50rem' }}>
+              <div className="about-me-typography">
 
-          <Paper elevation={3} className="about-me-paper">
-            <Typography>
+                Hello! My name is Michael and I have lived in beatiful Colorado for all my life.
+              </div>
+            </Paper>
+          </motion.div>
+          <motion.div
+            animate={{ rotate: 360 }}
+            transition={{ type: 'spring', delay: 1 }}
+          >
+            <Paper elevation={3} className="about-me-paper" sx={{ background: 'linear-gradient(#B5CB15, green)', width: '50rem' }}>
+              <div className="about-me-typography">
 
-              Hello! My name is Michael and I have lived in beatiful Colorado for all my life.
-            </Typography>
-          </Paper>
-          <Paper elevation={3} className="about-me-paper">
-            <Typography>
-
-              After 10 years in the healthcare industry I made a pivot into software, and couldn't
-              be happier!
-              What I enjoy most about software is the problem-solving aspect.
-              I pride myself on critical thinking and figuring out solutions to problems that need
-              solving.
-            </Typography>
-          </Paper>
+                After 10 years in the healthcare industry I made a pivot into software, and couldn't
+                be happier!
+                What I enjoy most about software is the problem-solving aspect.
+                I pride myself on critical thinking and figuring out solutions to problems that need
+                solving.
+              </div>
+            </Paper>
+          </motion.div>
+          <motion.div
+            animate={{ rotate: -360 }}
+            transition={{ type: 'spring', delay: 2 }}
+          >
+            <Paper elevation={3} className="about-me-paper" sx={{ background: 'linear-gradient(blue, purple)', width: '50rem' }}>
+              <div className="about-me-typography">
+                I love to travel, and being a software developer has given me the ability
+                to explore more, for which I am very grateful. This summer, I also started
+                playing golf, which has been great!
+                Always happy to connect, so feel free to reach out
+              </div>
+            </Paper>
+          </motion.div>
         </div>
       </div>
     </div>
