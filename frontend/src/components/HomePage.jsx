a53c951c7f5871ec4baf55824a5248d78ef4cd06
Michael Light <mklight06@gmail.com>
2024-03-28 11:13:31 -0600

    new UI branch, adding tailwind

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Heading from './Heading';
 import Menu from './Menu';
+import { Container } from '@mui/material';
 
 export default function HomePage() {
   return (
     <div id="main">
-
-      <div className="menu-sidebar">
-        <Menu />
-      </div>
-      <div>
-        <Heading />
-      </div>
+      <Container>
+        <div className="menu-sidebar">
+          <Menu />
+        </div>
+        <div>
+          <Heading />
+        </div>
+      </Container>
     </div>
   );
 }
